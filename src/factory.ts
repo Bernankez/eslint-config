4fe8e24092be720c59c1f99c5082246f9ea71de3
cole <bernankeic@gmail.com>
2024-08-30 09:43:22 +0800

    feat: add more unicorn rules, allow to configure using `recommended` rules

--- src/factory.ts
+++ src/factory.ts
@@ -95,6 +95,7 @@ export function bernankez(
     solid: enableSolid = false,
     svelte: enableSvelte = false,
     typescript: enableTypeScript = isPackageExists("typescript"),
+    unicorn: enableUnicorn = true,
     unocss: enableUnoCSS = false,
     vue: enableVue = VuePackages.some(i => isPackageExists(i)),
   } = options;
@@ -146,11 +147,14 @@ export function bernankez(
     imports({
       stylistic: stylisticOptions,
     }),
-    unicorn(),
 
     // Optional plugins (installed but not enabled by default)
     perfectionist(),
   );
+
+  if (enableUnicorn) {
+    configs.push(unicorn(enableUnicorn === true ? {} : enableUnicorn));
+  }
 
   if (enableVue) {
     componentExts.push("vue");
