518127aabd5ed57895d9512188d27e01a345ae76
cole <bernankeic@gmail.com>
2025-02-10 14:25:48 +0800

    feat: change editor rules from disable to non-fixable

--- src/factory.ts
+++ src/factory.ts
@@ -323,6 +323,16 @@ export function bernankez(
       .renamePlugins(defaultPluginRenaming);
   }
 
+  if (isInEditor) {
+    composer = composer.disableRulesFix([
+      "unused-imports/no-unused-imports",
+      "test/no-only-tests",
+      "prefer-const",
+    ], {
+      builtinRules: () => import(["eslint", "use-at-your-own-risk"].join("/")).then(r => r.builtinRules),
+    });
+  }
+
   return composer;
 }
 
