173dfb74ece67eac81631b9a1dee9e188e110578
cole <bernankeic@gmail.com>
2025-02-10 14:14:36 +0800

    feat: update react rules

--- src/configs/formatters.ts
+++ src/configs/formatters.ts
@@ -240,9 +240,9 @@ export async function formatters(
           "error",
           formater === "prettier"
             ? mergePrettierOptions(prettierOptions, {
-              embeddedLanguageFormatting: "off",
-              parser: "markdown",
-            })
+                embeddedLanguageFormatting: "off",
+                parser: "markdown",
+              })
             : {
                 ...dprintOptions,
                 language: "markdown",
